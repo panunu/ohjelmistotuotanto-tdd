ca2be168c0b71dcd623bb59fba2c7222671b5519
Panu Leppäniemi <me@panuleppaniemi.com>
2012-10-14 17:17:19 +0300

    Initial

---
+++ test/tdd/GreeterTest.java
@@ -0,0 +1,26 @@
+package tdd;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+public class GreeterTest {
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
